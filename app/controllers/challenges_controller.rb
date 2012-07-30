150d5cf2ac3f6663ededc3ddddfb4e1c43536967
Tyler Long <tyler4long@gmail.com>
2012-07-30 22:35:04 +0800

    Can create and read challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -2,4 +2,22 @@ class ChallengesController < ApplicationController
   def index
     @challenges = Challenge.all
   end
+
+  def new
+    @challenge = Challenge.new
+  end
+
+
+  def create
+    @challenge = Challenge.new(params[:challenge])
+    if @challenge.save
+      redirect_to @challenge, notice: "Successfully created challenge."
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @challenge = Challenge.find(params[:id])
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def field(f, attribute, options = {})
+    label_name = options.delete(:label)
+    type = options.delete(:type) || :text_field
+    content_tag(:div, (f.label(attribute, label_name) + f.send(type, attribute, options)), :class => "field")
+  end
 end
